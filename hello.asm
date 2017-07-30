f0bee2b24e1a402e86e49d9a2b4654780ef5634c
Rob Garcia <rgarcia@rgcoding.com>
2017-07-29 22:58:02 -0400

    Converted the executable file into a bootloader
    Modified the hello.asm code by changing the offset (from 0x100 to 0x7C00) and adding a boot signature (AA 55), turning it into a bootloader.
    hello.img is the disk image file for VirtualBox

--- hello.asm
+++ hello.asm
@@ -1,14 +1,16 @@
-﻿BITS 16                   ;Let NASM know to use 16 bit mode
-org 0x100                 ;.com files always start 256 bytes into the segment
-mov si,msg                ;Move the address of msg into the SI register
-mov ah,0x0e               ;Teletype function code for INT 10
-startloop:                ;Loop start point label
-lodsb                     ;Load the char at the SI address into AL and go to
-                          ;the next char
-cmp al,0x00               ;Compare AL to 0
-je endloop                ;Jump to endloop if AL equals 0
-int 0x10                  ;Call INT 10 to print the char in AL to the screen
-jmp startloop             ;Jump back to the loop start point
-endloop:                  ;End of the loop label
-ret                       ;Quit the program
+﻿BITS 16                   ;Let NASM know to use 16 bit mode
+org 0x7C00                ;Bootloader memory offset
+mov si,msg                ;Move the address of msg into the SI register
+mov ah,0x0e               ;Teletype function code for INT 10
+startloop:                ;Loop start point label
+lodsb                     ;Load the char at the SI address into AL and go to
+                          ;the next char
+cmp al,0x00               ;Compare AL to 0
+je endloop                ;Jump to endloop if AL equals 0
+int 0x10                  ;Call INT 10 to print the char in AL to the screen
+jmp startloop             ;Jump back to the loop start point
+endloop:                  ;End of the loop label
+ret                       ;Quit the program
 msg: db 'hello, world',0  ;Bytes to print
+times 510 - ($-$$) db 0   ;Pad the rest of the bootloader with zeros
+dw 0xAA55                 ;Add the Boot signature
